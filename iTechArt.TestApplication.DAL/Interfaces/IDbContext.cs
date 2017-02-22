71dc8c0f72c3b70adde4c26bcd3b452487fb5188
Olga Mikholenko <olga.mikholenko@itechart-group.com>
2017-02-22 19:28:44 +0300

    add BaseRepository

---
+++ iTechArt.TestApplication.DAL/Interfaces/IDbContext.cs
@@ -0,0 +1,16 @@
+﻿using iTechArt.TestApplication.DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt.TestApplication.DAL.Interfaces
+{
+	public interface IDbContext
+	{
+		IDbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity;
+		int SaveChanges();
+	}
+}
---
+++ iTechArt.TestApplication.DAL/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using iTechArt.TestApplication.DAL.EF;
+using iTechArt.TestApplication.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+
+namespace iTechArt.TestApplication.DAL.Repositories
+{
+	public abstract class Repository<T>: IRepository<T> where T: BaseEntity
+	{
+		private readonly IDbContext _context;
+		private IDbSet<T> _entities;
+
+		public Repository(IDbContext context)
+		{
+			this._context = context;
+		}
+
+		protected virtual IDbSet<T> Entities
+		{
+			get { return _entities ?? (_entities = _context.Set<T>()); }
+		}
+		public IEnumerable<T> GetAll() {
+			return Entities;
+		}
+
+		public void Add(T item)
+		{
+			Entities.Add(item);
+			_context.SaveChanges();
+		}
+
+		public void Update(T depot)
+		{
+		
+			//Entities.
+			_context.SaveChanges();
+		}
+	}
+}
